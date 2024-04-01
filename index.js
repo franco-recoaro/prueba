765cb3162fc9265dadccfc8aff45d6d9495b7b6b
Franco <flrecoaro@gmail.com>
2024-04-01 16:27:43 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const imagenes = document.querySelectorAll('#slideshow img');
+const botones = document.querySelectorAll('.botones button');
+
+let indiceActual = 0;
+
+function mostrarImagen(indice) {
+  imagenes.forEach((imagen, i) => {
+    imagen.style.display = i === indice ? 'block' : 'none';
+  });
+  botones.forEach((boton, i) => {
+    boton.classList.remove('activo');
+  });
+  botones[indice].classList.add('activo');
+}
+
+mostrarImagen(indiceActual);
+
+botones.forEach((boton, i) => {
+  boton.addEventListener('click', () => {
+    indiceActual = i;
+    mostrarImagen(indiceActual);
+  });
+});
